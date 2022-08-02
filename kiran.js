7aff7dcfa0293df9496b581d95ecda4b8748c87a
saikiran yalavarthi <107987848+saigithubkiran@users.noreply.github.com>
2022-08-02 09:24:05 +0530

    Add files via upload

---
+++ kiran.js
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <pre>
+        const Multiply_two_number = (a,b) => {
+            return (a*b); 
+         };
+         
+        console.log(Multiply_two_number(2,5));
+        </pre>
+        <pre>
+
+        const Sum = (A, B, C) => 
+{
+  let res1;
+  res1 = (A+B+C);
+  return res1;
+};
+
+const Average = (A, B, C) => 
+{
+ let res2;
+ res2 = ((A+B+C)/3);
+ return res2;
+};
+    </pre>
+</body>
+</html>
